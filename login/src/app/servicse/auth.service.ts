12e2c54ec48d5f11b182861b0555a288eeac1aca
abeer19955 <abeerali19955@gmail.com>
2023-08-21 12:14:55 +0400

    ss

---
+++ login/src/app/servicse/auth.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private http:HttpClient) { 
+
+  }
+  apiurl='http://localhost:3000/user';
+
+  RegisterUser(inputdata:any){
+    return this.http.post(this.apiurl,inputdata)
+  }
+  GetUserbyCode(id:any){
+    return this.http.get(this.apiurl+'/'+id);
+  }
+  Getall(){
+    return this.http.get(this.apiurl);
+  }
+}
